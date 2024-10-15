4c6257c155430239da6e4144970fef4de20c492f
Siddireddi Shanmukha Sanjana <151708467+Sanjana-Siddireddi@users.noreply.github.com>
2024-10-15 14:20:18 +0530

    Assignment_BasicCalculator.java

--- Assignment_BasicCalculator.java
+++ Assignment_BasicCalculator.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.Scanner;
-public class BasicCalculator {
+public class Assignment_BasicCalculator {
     public static void main(String args[]) {
         double num1, num2;
         Scanner sc = new Scanner(System.in);
